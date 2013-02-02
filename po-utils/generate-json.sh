e41b69a9be0a7608e52ce39c2576a54fe7bb44ff
Simon Cross <hodgestar@gmail.com>
2013-02-02 12:26:35 +0200

    Fix up if statements in bash scripts and allow generate-json.sh to be used generically.

--- po-utils/generate-json.sh
+++ po-utils/generate-json.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-LANGS="en sw"
+PROJECT_DIR="$1"
+LANGS="$2"
+if [ "x$PROJECT_DIR" = "x" -o "x$LANGS" = "x" ] ; then
+    echo "Usage:" `basename "$0"` "<project_folder> <langs>"
+    echo "  e.g." `basename "$0"` ". 'en sw'"
+    exit 1
+fi
+
+SCRIPT_DIR="`dirname $0`"
+
 DOMAIN="messages"
-PROJECT_DIR="`dirname $0`/.."
 PO_DIR="$PROJECT_DIR/po"
 for lang in $LANGS ; do
    LANG_DIR="$PO_DIR/$lang/LC_MESSAGES"
    echo "$lang ($LANG_DIR)"
-   "$PROJECT_DIR/utils/po2json.py" --lang "$lang" --indent 2 "$DOMAIN" "$PO_DIR" > "$LANG_DIR/$DOMAIN.json"
+   "$SCRIPT_DIR/po2json.py" --lang "$lang" --indent 2 "$DOMAIN" "$PO_DIR" > "$LANG_DIR/$DOMAIN.json"
 done
