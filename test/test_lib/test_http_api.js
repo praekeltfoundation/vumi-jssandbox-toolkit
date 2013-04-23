c0f18c9b741fc5f9a36a4a41870c01b2b3b8a94d
Simon Cross <hodgestar@gmail.com>
2013-04-23 15:23:36 +0200

    Add api_get test.

--- test/test_lib/test_http_api.js
+++ test/test_lib/test_http_api.js
@@ -1,5 +1,7 @@
 var assert = require("assert");
+var sinon = require("sinon");
 var http_api = require("../../lib/http_api.js");
+
 
 var HttpApi = http_api.HttpApi;
 var JsonApi = http_api.JsonApi;
@@ -7,7 +9,30 @@ var JsonApi = http_api.JsonApi;
 
 function DummyIm() {
     var self = this;
+    self.log = sinon.spy();
+    self.api = { request: sinon.stub() };
+
+    self.requestSucceeds = function(body) {
+        self.api.request.callsArgWith(2, {
+            success: true,
+            code: 200,
+            body: body,
+            reason: null
+        });
+    };
+
+    self.checkRequest = function(method, url, headers) {
+        assert(self.api.request.calledOnce);
+        assert(self.api.request.calledWith(
+            method,
+            sinon.match({
+                url: url,
+                headers: headers
+            })
+        ));
+    };
 }
+
 
 
 describe("test HttpApi", function() {
@@ -15,6 +40,15 @@ describe("test HttpApi", function() {
 
     it("should be constructable", function() {
         var api = new HttpApi(im);
+    });
+
+    it("should respond when api_get is called", function(done) {
+        var api = new HttpApi(im);
+        im.requestSucceeds("foo");
+        var p = api.api_get("http://www.example.com/");
+        im.checkRequest('http.get', "http://www.example.com/", {});
+        p.add_callback(function (r) { assert.equal(r, "foo"); });
+        p.add_callback(done);
     });
 })
 
