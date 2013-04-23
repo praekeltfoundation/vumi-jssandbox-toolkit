47950d39edc5f3b43d703d15e1e15c257e8db374
Simon Cross <hodgestar@gmail.com>
2013-04-23 23:22:35 +0200

    Refactor tests.

--- test/test_lib/test_test_utils.js
+++ test/test_lib/test_test_utils.js
@@ -36,36 +36,44 @@ describe("ImTester.check_state", function() {
     var api = mk_app();
     var tester = new test_utils.ImTester(api);
 
-    it("should pass on correct next_state and response", function() {
-        tester.check_state({
+    function mk_opts(extra_opts) {
+        var opts = {
             user: {},
             content: null,
             next_state: "intro",
             response: "Type something:",
-        });
+        };
+        for (var k in extra_opts) {
+            opts[k] = extra_opts[k];
+        }
+        return opts;
+    }
+
+    function succeed_with(good_opts) {
+        var opts = mk_opts(good_opts);
+        tester.check_state(opts);
+    }
+
+    function fail_with(bad_opts) {
+        var opts = mk_opts(bad_opts);
+        assert.throws(function () {tester.check_state(opts);},
+                      assert.AssertionError);
+    }
+
+    it("should pass on correct next_state and response", function() {
+        succeed_with({});
     });
 
     it("should fail on incorrect next_state", function() {
-        assert.throws(function () {
-            tester.check_state({
-                user: {},
-                content: null,
-                next_state: "unknown",
-                response: "Type something:",
-            });
-        }, assert.AssertionError);
+        fail_with({next_state: "unknown"});
     });
 
     it("should fail on incorrect response", function() {
-        assert.throws(function () {
-            tester.check_state({
-                user: {},
-                content: null,
-                next_state: "intro",
-                response: "Type something else:",
-                log_on_error: false
-            });
-        }, assert.AssertionError);
+        fail_with({response: "Type something else:"});
+    });
+
+    it("should fail if response is too long", function() {
+        fail_with({max_content_length: 2});
     });
 });
 
@@ -74,19 +82,33 @@ describe("ImTester.check_close", function() {
     var api = mk_app();
     var tester = new test_utils.ImTester(api);
 
-    it("should pass on correct next_state", function() {
-        tester.check_close({
+    function mk_opts(extra_opts) {
+        var opts = {
             user: {},
-            next_state: "intro"
-        });
+            next_state: "intro",
+        };
+        for (var k in extra_opts) {
+            opts[k] = extra_opts[k];
+        }
+        return opts;
+    }
+
+    function succeed_with(good_opts) {
+        var opts = mk_opts(good_opts);
+        tester.check_close(opts);
+    }
+
+    function fail_with(bad_opts) {
+        var opts = mk_opts(bad_opts);
+        assert.throws(function () {tester.check_close(opts);},
+                      assert.AssertionError);
+    }
+
+    it("should pass on correct next_state", function() {
+        succeed_with({});
     });
 
     it("should fail on incorrect next_state", function() {
-        assert.throws(function () {
-            tester.check_close({
-                user: {},
-                next_state: "unknown"
-            });
-        }, assert.AssertionError);
+        fail_with({next_state: "unknown"});
     });
 });
