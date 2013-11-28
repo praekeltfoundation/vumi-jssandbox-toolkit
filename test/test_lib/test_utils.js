75b6b6306b87de9a27dd6454235b67e952a678ce
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-28 17:35:23 +0200

    Change call_possible_function to not return a promise

--- test/test_lib/test_utils.js
+++ test/test_lib/test_utils.js
@@ -5,46 +5,17 @@ var utils = require("../../lib/utils");
 
 describe("utils", function() {
     describe("call_possible_function", function() {
-        it("should handle sync functions", function(done) {
-            utils
-                .call_possible_function(
-                    function(b, c) { return this.a + b + c; },
-                    {a: 'foo'},
-                    ['bar', 'baz'])
-                .then(function(result) {
-                    assert.equal(result, 'foobarbaz');
-                })
-                .then(done, done);
+        it("should handle functions", function() {
+          assert.equal(
+              utils.call_possible_function(
+                  function(b, c) { return this.a + b + c; },
+                  {a: 'foo'},
+                  ['bar', 'baz']),
+              'foobarbaz');
         });
 
-        it("should handle async functions", function(done) {
-            utils
-                .call_possible_function(
-                    function(b, c) {
-                        var d = Q.defer();
-
-                        var self = this;
-                        setImmediate(function() {
-                            return d.resolve(self.a + b + c);
-                        });
-
-                        return d.promise;
-                    },
-                    {a: 'foo'},
-                    ['bar', 'baz'])
-                .then(function(result) {
-                    assert.equal(result, 'foobarbaz');
-                })
-                .then(done, done);
-        });
-
-        it("should handle non-functions", function(done) {
-            utils
-                .call_possible_function('foo')
-                .then(function(result) {
-                    assert.equal(result, 'foo');
-                })
-                .then(done, done);
+        it("should handle non-functions", function() {
+            assert.equal(utils.call_possible_function('foo'), 'foo');
         });
     });
 
