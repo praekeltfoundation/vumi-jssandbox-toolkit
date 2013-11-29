898831f5582ca72f5d9de86175915f08e42ffa4a
justinvdm <justinvdm.0.1.0@gmail.com>
2013-11-29 15:49:15 +0200

    Opts, promises and documenting for EndState

---
+++ test/test_lib/test_states/test_end.js
@@ -0,0 +1,63 @@
+var assert = require("assert");
+var vumigo = require("../../../lib");
+
+
+var DummyIm = vumigo.test_utils.DummyIm;
+var EndState = vumigo.states.EndState;
+
+
+describe("EndState", function () {
+    var im;
+    var state;
+
+    beforeEach(function () {
+        im = new DummyIm();
+        state = new EndState('state-1', 'state-2', 'hello goodbye');
+        state.setup_state(im);
+    });
+
+    describe(".input_event", function() {
+        it("should set the user's current state to the next state",
+        function(done) {
+            assert.strictEqual(im.user.current_state, null);
+
+            state
+                .input_event('A lemon')
+                .then(function() {
+                    assert.equal(im.user.current_state, 'state-2');
+                })
+                .then(done, done);
+        });
+    });
+
+    describe(".new_session_event", function() {
+        it("should fake an input event", function(done) {
+            assert.strictEqual(im.user.current_state, null);
+
+            state
+                .new_session_event()
+                .then(function() {
+                    assert.equal(im.user.current_state, 'state-2');
+                })
+                .then(done, done);
+        });
+    });
+
+    describe(".translate", function() {
+        var i18n;
+
+        beforeEach(function() {
+            i18n = {
+                gettext: function(s) {
+                    return s.toUpperCase();
+                }
+            };
+        });
+
+        it("should translate the state's text", function() {
+            assert.equal(state.display(), 'hello goodbye');
+            state.translate(i18n);
+            assert.equal(state.display(), 'HELLO GOODBYE');
+        });
+    });
+});
