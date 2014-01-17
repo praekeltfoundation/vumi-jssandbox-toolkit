3fb3db229705660d40a247cbe89376b250ba50c5
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-17 15:39:53 +0200

    Get the new SandboxConfig and IMConfig structures working and tested

--- test/fixtures/simple-config.js
+++ test/fixtures/simple-config.js
@@ -1,23 +1,25 @@
 module.exports = {
     'translation.af': JSON.stringify({
-        "" : {
-            "domain" : "messages",
-            "lang"   : "af",
-            "plural_forms" : "nplurals=2; plural=(n != 1);"
+        '' : {
+            'domain' : 'messages',
+            'lang'   : 'af',
+            'plural_forms' : 'nplurals=2; plural=(n != 1);'
         },
-        "yes" : [null, "ja"],
-        "no" : [null, "nee"]
+        'yes' : [null, 'ja'],
+        'no' : [null, 'nee']
     }),
     'translation.jp': JSON.stringify({
-        "" : {
-            "domain" : "messages",
-            "lang"   : "af",
-            "plural_forms" : "nplurals=2; plural=(n != 1);"
+        '' : {
+            'domain' : 'messages',
+            'lang'   : 'af',
+            'plural_forms' : 'nplurals=2; plural=(n != 1);'
         },
-        "yes" : [null, "hai"],
-        "no" : [null, "iie"]
+        'yes' : [null, 'hai'],
+        'no' : [null, 'iie']
     }),
     config: JSON.stringify({
+        lerp: 'larp',
         metric_store_name: 'luke_the_store'
-    })
+    }),
+    foo: JSON.stringify({bar: 'baz'})
 };
---
+++ test/test_lib/test_config.js
@@ -0,0 +1,79 @@
+var assert = require("assert");
+
+var vumigo = require("../../lib");
+var test_utils = vumigo.test_utils;
+var SandboxConfig = vumigo.config.SandboxConfig;
+var IMConfig = vumigo.config.IMConfig;
+
+
+describe("SandboxConfig", function() {
+    var im;
+    var config;
+
+    beforeEach(function(done) {
+        test_utils.make_im().then(function(new_im) {
+            im = new_im;
+            config = im.sandbox_config;
+        }).nodeify(done);
+    });
+
+    describe(".setup", function() {
+        it("should emit a 'setup' event", function(done) {
+            config = new SandboxConfig(im);
+
+            config.on('setup', function() {
+                done();
+            });
+
+            config.setup();
+        });
+    });
+    
+    describe(".get", function() {
+        it("should retrieve the config value", function(done) {
+            config.get('foo').then(function(value) {
+                assert.deepEqual(JSON.parse(value), {bar: 'baz'});
+            }).nodeify(done);
+        });
+
+        describe("if the 'json' option is true", function() {
+            it("should parse the config value as JSON", function(done) {
+                config.get('foo', {json: true}).then(function(value) {
+                    assert.deepEqual(value, {bar: 'baz'});
+                }).nodeify(done);
+            });
+        });
+    });
+});
+
+
+describe("IMConfig", function() {
+    var im;
+    var config;
+
+    beforeEach(function(done) {
+        test_utils.make_im().then(function(new_im) {
+            im = new_im;
+            config = im.config;
+        }).nodeify(done);
+    });
+
+    describe(".setup", function() {
+        it("should emit a 'setup' event", function(done) {
+            config = new IMConfig(im);
+
+            config.on('setup', function() {
+                done();
+            });
+
+            config.setup();
+        });
+
+        it("setup the config from its value in the sandbox config",
+        function(done) {
+            config.setup().then(function() {
+                assert.equal(config.get('lerp'), 'larp');
+            }).nodeify(done);
+        });
+    });
+});
