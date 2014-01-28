2b148da62aec66efec7ed7de1021078c88373b6d
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-29 00:45:35 +0200

    Add basic validation for AppTester setup tasks

--- test/test_tester/test_setup.js
+++ test/test_tester/test_setup.js
@@ -6,6 +6,8 @@ var App = app.App;
 var tester = require('../../lib/tester/tester');
 var AppTester = tester.AppTester;
 
+var tasks = require('../../lib/tester/tasks');
+var TaskMethodError = tester.TaskMethodError;
 
 describe("AppTester Setup Tasks", function() {
     var app;
@@ -14,6 +16,38 @@ describe("AppTester Setup Tasks", function() {
     beforeEach(function() {
         app = new App('start');
         tester = new AppTester(app);
+    });
+
+    describe("if interaction tasks have already been scheduled", function() {
+        beforeEach(function() {
+            var interactions = tester.tasks.get('interactions');
+            interactions.methods.interact = function() {};
+            tester.tasks.attach();
+        });
+
+        it("should throw an error when scheduling setup tasks", function() {
+            tester.interact();
+
+            assert.throws(function() {
+                tester.setup();
+            }, TaskMethodError);
+        });
+    });
+
+    describe("if checking tasks have already been scheduled", function() {
+        beforeEach(function() {
+            var checks = tester.tasks.get('checks');
+            checks.methods.check = function() {};
+            tester.tasks.attach();
+        });
+
+        it("should throw an error when scheduling setup tasks", function() {
+            tester.check();
+
+            assert.throws(function() {
+                tester.setup();
+            }, TaskMethodError);
+        });
     });
 
     describe(".setup", function() {
