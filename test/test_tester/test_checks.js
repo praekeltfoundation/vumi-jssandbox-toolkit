a2abd7412554a239b1525e33f07a86e9c9675317
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-30 16:18:12 +0200

    Stub out the AppTester checking task tests

--- test/test_tester/test_checks.js
+++ test/test_tester/test_checks.js
@@ -49,7 +49,7 @@ describe("AppTester Check Tasks", function() {
     });
 
     describe(".check", function() {
-        it("should call the given checker function", function() {
+        it("should call the given function", function() {
             var called = false;
 
             return tester.input().check(function(im, api, app) {
@@ -61,5 +61,96 @@ describe("AppTester Check Tasks", function() {
                 assert(called);
             });
         });
+    });
+
+    describe(".check.interaction", function() {
+        it("should check the current state name");
+        it("should check the reply content if given");
+    });
+
+    describe(".check.user", function() {
+        describe(".check.user(obj)", function() {
+            it("should check the user's state if given");
+
+            it("should check the user's answers if given");
+
+            it("should check arbitrary user fields");
+
+            it("should throw an error if non-existant fields are given");
+        });
+
+
+        describe(".check.user(fn)", function() {
+            it("should call the function with the user instance");
+        });
+    });
+
+    describe(".check.user.answers", function() {
+        it("should check the user's answers");
+    });
+
+    describe(".check.user.answer", function() {
+        it("should check user's answer for the given state");
+    });
+
+    describe(".check.state", function() {
+        describe(".check.state(name, metadata)", function() {
+            it("should check the current state's name");
+            it("should check the current state's metadata if given");
+        });
+
+        describe(".check.state(obj)", function() {
+            it("should check the current state's name");
+            it("should check the current state's metadata if given");
+        });
+
+        describe(".check.state(fn)", function() {
+            it("should call the function with the state instance");
+        });
+    });
+
+    describe(".check.state.attrs", function() {
+        it("should check the current state's name");
+        it("should check the current state's metadata if given");
+    });
+
+    describe(".check.state.metadata", function() {
+        it("should check the current state's metadata");
+    });
+
+    describe(".check.reply", function() {
+        describe(".check.reply(content)", function() {
+            it("should check the content of the sent reply");
+            it("should throw an error an error if no reply was sent");
+            it("should throw an error if multiple replies were sent");
+        });
+
+        describe(".check.reply(obj)", function() {
+            it("should check the properties of the sent reply");
+            it("should throw an error an error if no reply was sent");
+            it("should throw an error if multiple replies were sent");
+        });
+
+        describe(".check.reply(fn)", function() {
+            it("should call the function with the state instance");
+            it("should throw an error an error if no reply was sent");
+            it("should throw an error if multiple replies were sent");
+        });
+    });
+
+    describe(".check.reply.content", function() {
+        it("should check the content of the sent reply");
+        it("should throw an error an error if no reply was sent");
+        it("should throw an error if multiple replies were sent");
+    });
+
+    describe(".check.reply.shorter_than", function() {
+        it("should check that the reply char count is below the given number");
+        it("should throw an error an error if no reply was sent");
+        it("should throw an error if multiple replies were sent");
+    });
+
+    describe(".check.reply.no_reply", function() {
+        it("should check that no reply was sent");
     });
 });
