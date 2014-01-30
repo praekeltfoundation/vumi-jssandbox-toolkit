6baa8a47a6824e0deb4534e2d1656f92ca99d2d8
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-30 02:24:21 +0200

    AppTester interaction task message sending tests

--- test/test_tester/test_interaction.js
+++ test/test_tester/test_interaction.js
@@ -12,10 +12,12 @@ var TaskMethodError = tester.TaskMethodError;
 describe("AppTester Interaction Tasks", function() {
     var app;
     var tester;
+    var tasks;
 
     beforeEach(function() {
         app = new App('start');
         tester = new AppTester(app);
+        tasks = tester.tasks.get('interactions');
     });
 
     describe("if checking tasks have already been scheduled", function() {
@@ -35,10 +37,56 @@ describe("AppTester Interaction Tasks", function() {
     });
 
     describe("message sending", function() {
-        it("should send the message into the sandbox");
-        it("should use the same shutdown handling as the sandbox");
-        it("should use the same error handling as the sandbox");
-        it("should use rethrow sandbox errors");
+        var msg;
+
+        beforeEach(function() {
+            msg = {
+                message_id: '1',
+                content: 'hello',
+                session_event: 'resume'
+            };
+        });
+
+        it("should send the message into the sandbox", function() {
+            return tasks.send(msg).then(function() {
+                assert.equal(tester.im.msg.message_id, '1');
+                assert.equal(tester.im.msg.content, 'hello');
+                assert.equal(tester.im.msg.session_event, 'resume');
+            });
+        });
+
+        it("should use the same shutdown handling as the im", function() {
+            var p = tester.im.once.resolved('im:shutdown');
+            return tasks.send(msg).thenResolve(p);
+        });
+
+        describe("when an error occurs in the sandbox", function() {
+            var error;
+
+            beforeEach(function() {
+                error = new Error(':(');
+
+                app.states.add('start', function() {
+                    throw new Error(':(');
+                });
+            });
+
+            it("should use the same error handling as the im", function() {
+                var p = tester.im.once.resolved('im:error');
+
+                return tasks
+                    .send(msg)
+                    .catch(function() {})
+                    .thenResolve(p);
+            });
+
+            it("should rethrow the error", function() {
+                return tasks.send(msg).catch(function(e) {
+                    assert.equal(e.message, ':(');
+                    assert.equal(error.message, e.message);
+                });
+            });
+        });
     });
 
     describe(".input", function() {
