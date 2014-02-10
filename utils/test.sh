cfa5bcf3b37a833d263c3b53b1d5bdbe167c4b10
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-10 19:23:06 +0200

    Update test script to first lint the files with jshint, then test

--- utils/test.sh
+++ utils/test.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-mocha --require "test/setup.js" `find "$@" -name "*.js"`
+#!/bin/bash -e
+files=`find "$@" -name "*.js"`
+./node_modules/.bin/jshint $files
+./node_modules/.bin/mocha --require "test/setup.js" $files
