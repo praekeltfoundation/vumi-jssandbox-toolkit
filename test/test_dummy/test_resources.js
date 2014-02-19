d60d1589409263449078ea5ce72a87cc67a4e550
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-19 11:08:27 +0200

    Tests for DummyResource's handler delegation

--- test/test_dummy/test_resources.js
+++ test/test_dummy/test_resources.js
@@ -45,6 +45,44 @@ var BadToyResource = DummyResource.extend(function(self) {
     };
 });
 
+describe("DummyResource", function() {
+    describe(".handle", function() {
+        it("should delegate to the corresponding handling", function() {
+            var resource = new ToyResource();
+
+            return resource
+                .handle({cmd: 'toy.foo'})
+                .then(function(result) {
+                    assert.deepEqual(result, {
+                        handler: 'foo',
+                        cmd: {cmd: 'toy.foo'}
+                    });
+                });
+        });
+
+        it("should fail if there is no handler for the command", function() {
+            var resource = new ToyResource();
+
+            return resource
+                .handle({cmd: 'toy.bar'})
+                .then(function(result) {
+                    assert(!result.success);
+                });
+        });
+
+        it("should treat unhandled exceptions as failure replies", function() {
+            var resource = new BadToyResource();
+
+            return resource
+                .handle({cmd: 'bad_toy.foo'})
+                .then(function(result) {
+                    assert(!result.success);
+                    assert.equal(result.reason, ':(');
+                });
+        });
+    });
+});
+
 describe("DummyResources", function() {
     describe(".add", function() {
         it("should add the resource", function() {
@@ -98,7 +136,7 @@ describe("DummyResources", function() {
                 });
         });
 
-        it("should fail if no such resource exists", function() {
+        it("should fail if there is no resource for the command", function() {
             var resources = new DummyResources();
             return resources
                 .handle({cmd: 'toy.foo'})
@@ -107,7 +145,7 @@ describe("DummyResources", function() {
                 });
         });
 
-        it("should fail if no such handler exists", function() {
+        it("should fail if there is no handler for the command", function() {
             var resources = new DummyResources();
             resources.add(new ToyResource());
 
