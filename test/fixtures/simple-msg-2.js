b6306db47cdd313b58e30931a924cc2c02cadbf8
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-21 00:44:46 +0200

    Re-organise fixtures and put them in lib/ (we are requiring them from lib/test_utils)

--- test/fixtures/simple-msg-2.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-    return {
-        from_addr: '+27123456789',
-        content: 'hello',
-        message_id: '2',
-        session_event: 'new',
-        helper_metadata: {}
-    };
-};
