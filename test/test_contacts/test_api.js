0f6f60b27dc7432d6ad4bbe7d820499161e75946
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-26 21:59:47 +0200

    Change Contact to use Model

--- test/test_contacts/test_api.js
+++ test/test_contacts/test_api.js
@@ -7,56 +7,6 @@ var ContactError = api.ContactError;
 
 
 describe("Contact", function() {
-    describe(".reset", function() {
-        it("should reset the contact using the given attributes", function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                extra: {ham: 'spam'},
-                groups: ['foo'],
-            });
-
-            contact.reset({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['bar']
-            });
-
-            assert.deepEqual(contact.serialize(), {
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                extra: {},
-                groups: ['bar']
-            });
-        });
-
-        it("should re-validate the contact", function() {
-            var validated = false;
-
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo'
-            });
-
-            contact.validate = function() {
-                validated = true;
-            };
-
-            assert(!validated);
-            contact.reset({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                extra: {ham: 'spam'}
-            });
-            assert(validated);
-        });
-    });
-
     describe(".validate", function() {
         it("should throw an error for non-string msisdns", function() {
             var contact = new Contact({
@@ -65,7 +15,7 @@ describe("Contact", function() {
                 user_account: 'user_foo'
             });
 
-            contact.attrs.msisdn = null;
+            contact.msisdn = null;
 
             assert.throws(
                 function() { contact.validate(); },
@@ -87,7 +37,7 @@ describe("Contact", function() {
                 user_account: 'user_foo',
             });
 
-            contact.attrs.groups.push(null);
+            contact.groups.push(null);
 
             assert.throws(
                 function() { contact.validate(); },
@@ -109,7 +59,7 @@ describe("Contact", function() {
                 user_account: 'user_foo',
             });
 
-            contact.attrs.extra.spam = null;
+            contact.extra.spam = null;
 
             assert.throws(
                 function() { contact.validate(); },
@@ -122,102 +72,6 @@ describe("Contact", function() {
 
                     return true;
                 });
-        });
-    });
-
-    describe(".serialize", function() {
-        it("should validate the contact", function() {
-            var validated = false;
-
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-            });
-
-            contact.validate = function() {
-                validated = true;
-            };
-
-            assert(!validated);
-            contact.serialize();
-            assert(validated);
-        });
-
-        it("should serialize the contact", function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['group_1'],
-                extra: {ham: 'spam'},
-            });
-
-            assert.deepEqual(contact.serialize(), {
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['group_1'],
-                extra: {ham: 'spam'},
-            });
-        });
-
-        it("should return a shallow copy of the contact's attributes",
-        function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['group_1'],
-                extra: {ham: 'spam'}
-            });
-
-            var data = contact.serialize();
-            data.key = '456';
-
-            assert.deepEqual(contact.attrs, {
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['group_1'],
-                extra: {ham: 'spam'}
-            });
-        });
-
-        it("should return a shallow copy of the contact's groups",
-        function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['group_1'],
-                extra: {ham: 'spam'}
-            });
-
-            var data = contact.serialize();
-            data.groups.push('group_2');
-
-            assert.deepEqual(
-                contact.attrs.groups,
-                ['group_1']);
-        });
-
-        it("should return a shallow copy of the contact's extras",
-        function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-                groups: ['group_1'],
-                extra: {ham: 'spam'}
-            });
-
-            var data = contact.serialize();
-            data.extra.lamb = 'ram';
-
-            assert.deepEqual(
-                contact.attrs.extra,
-                {ham: 'spam'});
         });
     });
 });
