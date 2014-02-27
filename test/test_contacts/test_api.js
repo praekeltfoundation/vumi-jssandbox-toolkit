7d810bb7383c4d6a8362beff11a05724a8c451c9
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-27 16:31:10 +0200

    Stub out ContactStore tests

--- test/test_contacts/test_api.js
+++ test/test_contacts/test_api.js
@@ -6,95 +6,133 @@ var Contact = api.Contact;
 var ContactError = api.ContactError;
 
 
-describe("Contact", function() {
-    describe(".do.validate", function() {
-        it("should throw an error for non-string msisdns", function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo'
+describe("contacts.api", function() {
+    describe("Contact", function() {
+        describe(".do.validate", function() {
+            it("should throw an error for non-string msisdns", function() {
+                var contact = new Contact({
+                    key: '123',
+                    msisdn: '+27123',
+                    user_account: 'user_foo'
+                });
+
+                contact.msisdn = null;
+
+                assert.throws(
+                    function() { contact.do.validate(); },
+                    function(error) {
+                        assert(error instanceof ContactError);
+                        assert.equal(
+                            error.message,
+                            ["Contact has an msisdn of type 'object' instead of",
+                             "'string': null"].join(' '));
+
+                        return true;
+                    });
             });
 
-            contact.msisdn = null;
-
-            assert.throws(
-                function() { contact.do.validate(); },
-                function(error) {
-                    assert(error instanceof ContactError);
-                    assert.equal(
-                        error.message,
-                        ["Contact has an msisdn of type 'object' instead of",
-                         "'string': null"].join(' '));
-
-                    return true;
+            it("should throw an error for non-string groups", function() {
+                var contact = new Contact({
+                    key: '123',
+                    msisdn: '+27123',
+                    user_account: 'user_foo',
                 });
+
+                contact.groups.push(null);
+
+                assert.throws(
+                    function() { contact.do.validate(); },
+                    function(error) {
+                        assert(error instanceof ContactError);
+                        assert.equal(
+                            error.message,
+                            ["Contact has a group of type 'object' instead of",
+                             "'string': null"].join(' '));
+
+                        return true;
+                    });
+            });
+
+            it("should throw an error for non-string extra values", function() {
+                var contact = new Contact({
+                    key: '123',
+                    msisdn: '+27123',
+                    user_account: 'user_foo',
+                });
+
+                contact.extra.spam = null;
+
+                assert.throws(
+                    function() { contact.do.validate(); },
+                    function(error) {
+                        assert(error instanceof ContactError);
+                        assert.equal(
+                            error.message,
+                            ["Contact extra 'spam' has a value of type 'object'",
+                             "instead of 'string': null"].join(' '));
+
+                        return true;
+                    });
+            });
+
+            it("should throw an error for non-string subscription values",
+            function() {
+                var contact = new Contact({
+                    key: '123',
+                    msisdn: '+27123',
+                    user_account: 'user_foo',
+                });
+
+                contact.subscriptions.conv3 = null;
+
+                assert.throws(
+                    function() { contact.do.validate(); },
+                    function(error) {
+                        assert(error instanceof ContactError);
+                        assert.equal(
+                            error.message,
+                            ["Contact subscription 'conv3' has a value of type",
+                             "'object' instead of 'string': null"].join(' '));
+
+                        return true;
+                    });
+            });
+        });
+    });
+
+    describe("ContactStore", function() {
+        describe(".setup", function() {
+            it("should emit a setup event");
         });
 
-        it("should throw an error for non-string groups", function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-            });
-
-            contact.groups.push(null);
-
-            assert.throws(
-                function() { contact.do.validate(); },
-                function(error) {
-                    assert(error instanceof ContactError);
-                    assert.equal(
-                        error.message,
-                        ["Contact has a group of type 'object' instead of",
-                         "'string': null"].join(' '));
-
-                    return true;
-                });
+        describe(".request", function() {
+            it("should makea raw request to the api's contacts resource");
         });
 
-        it("should throw an error for non-string extra values", function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-            });
-
-            contact.extra.spam = null;
-
-            assert.throws(
-                function() { contact.do.validate(); },
-                function(error) {
-                    assert(error instanceof ContactError);
-                    assert.equal(
-                        error.message,
-                        ["Contact extra 'spam' has a value of type 'object'",
-                         "instead of 'string': null"].join(' '));
-
-                    return true;
-                });
+        describe(".get", function() {
+            it("should create non-existant contacts if asked");
+            it("should accept fall back to the store's delivery class");
         });
 
-        it("should throw an error for non-string subscription values",
-        function() {
-            var contact = new Contact({
-                key: '123',
-                msisdn: '+27123',
-                user_account: 'user_foo',
-            });
+        describe(".get_by_key", function() {
+            it("should retrieve a contact by its key");
+        });
 
-            contact.subscriptions.conv3 = null;
+        describe(".for_user", function() {
+            it("should create a contact for the user if necessary");
+            it("should retrieve the user's contact if it exists");
+        });
 
-            assert.throws(
-                function() { contact.do.validate(); },
-                function(error) {
-                    assert(error instanceof ContactError);
-                    assert.equal(
-                        error.message,
-                        ["Contact subscription 'conv3' has a value of type",
-                         "'object' instead of 'string': null"].join(' '));
+        describe(".search", function() {
+            it("should retrieve the matching contacts");
+        });
 
-                    return true;
-                });
+        describe(".search_keys", function() {
+            it("should retrieve the matching keys");
+        });
+
+        describe(".save", function() {
+            it("should save the given contact");
         });
     });
 });
