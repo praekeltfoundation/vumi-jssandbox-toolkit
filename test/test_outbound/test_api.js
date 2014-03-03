34b52166d7f2a4e9be981f59b36683bea723e6a3
justinvdm <justinvdm.0.1.0@gmail.com>
2014-03-03 19:04:11 +0200

    Implement OutboundHelper (involved changing send_to() to send(), which now accepts an options object only)

--- test/test_outbound/test_api.js
+++ test/test_outbound/test_api.js
@@ -2,29 +2,153 @@ var assert = require('assert');
 
 var vumigo = require("../../lib");
 var test_utils = vumigo.test_utils;
-var DummyApi = vumigo.dummy.api.DummyApi;
+var OutboundHelper = vumigo.outbound.api.OutboundHelper;
 
 
 describe("outbound.api", function() {
+    var im;
+
+    beforeEach(function() {
+        return test_utils.make_im().then(function(new_im) {
+            im = new_im;
+        });
+    });
+
     describe("OutboundHelper", function() {
         describe(".setup", function() {
-            it("should emit a 'setup' event");
+            it("should emit a 'setup' event", function() {
+                var outbound = new OutboundHelper(im);
+                var p = outbound.once.resolved('setup');
+
+                return outbound.setup().then(function() {
+                    assert(p.isFulfilled());
+                });
+            });
         });
 
-        describe(".send_to", function() {
-            describe(".send_to(to_addr, endpoint)", function() {
-                it("should send to the given address");
+        describe(".send", function() {
+            describe("when an address is given", function() {
+                it("should send to the address", function() {
+                    return im
+                        .outbound.send({
+                            to: '+273123',
+                            endpoint: 'sms',
+                            content: 'hello!'
+                        })
+                        .then(function() {
+                            var sends = im.api.outbound.store;
+                            assert.equal(sends.length, 1);
+                            assert.deepEqual(sends[0], {
+                                to_addr: '+273123',
+                                content: 'hello!',
+                                endpoint: 'sms'
+                            });
+                        });
+                });
             });
 
-            describe(".send_to(contact, endpoint, opts)", function() {
-                it("should send to the given contact");
-                it("should fall back to the configured delivery class");
-                it("should finally fall back to the helper's delivery class");
+            describe("when a contact is given", function() {
+                it("should send to the contact", function() {
+                    return im
+                        .contacts.create({msisdn: '+273123'})
+                        .then(function(contact) {
+                            return im.outbound.send({
+                                to: contact,
+                                content: 'hello!',
+                                endpoint: 'sms',
+                                delivery_class: 'sms'
+                            });
+                        })
+                        .then(function() {
+                            var sends = im.api.outbound.store;
+                            assert.equal(sends.length, 1);
+                            assert.deepEqual(sends[0], {
+                                to_addr: '+273123',
+                                content: 'hello!',
+                                endpoint: 'sms'
+                            });
+                        });
+                });
+
+                it("should fall back to the endpoint's delivery class",
+                function() {
+                    var outbound = new OutboundHelper(im);
+
+                    return outbound
+                        .setup({
+                            endpoints: {
+                                'twitter_endpoint': {delivery_class: 'twitter'}
+                            }
+                        })
+                        .then(function() {
+                            return im
+                                .contacts.create({twitter_handle: '@foo'})
+                                .then(function(contact) {
+                                    return outbound.send({
+                                        to: contact,
+                                        content: 'hello!',
+                                        endpoint: 'twitter_endpoint',
+                                    });
+                                })
+                                .then(function() {
+                                    var sends = im.api.outbound.store;
+                                    assert.equal(sends.length, 1);
+                                    assert.deepEqual(sends[0], {
+                                        to_addr: '@foo',
+                                        content: 'hello!',
+                                        endpoint: 'twitter_endpoint'
+                                    });
+                                });
+                    });
+                });
+
+                it("should finally fall back to the helper's delivery class",
+                function() {
+                    var outbound = new OutboundHelper(im);
+
+                    return outbound
+                        .setup({delivery_class: 'twitter'})
+                        .then(function() {
+                            return im
+                                .contacts.create({twitter_handle: '@foo'})
+                                .then(function(contact) {
+                                    return outbound.send({
+                                        to: contact,
+                                        content: 'hello!',
+                                        endpoint: 'twitter',
+                                    });
+                                })
+                                .then(function() {
+                                    var sends = im.api.outbound.store;
+                                    assert.equal(sends.length, 1);
+                                    assert.deepEqual(sends[0], {
+                                        to_addr: '@foo',
+                                        content: 'hello!',
+                                        endpoint: 'twitter'
+                                    });
+                                });
+                    });
+                });
             });
         });
 
         describe(".send_to_user", function() {
-            it("should send to the user");
+            it("should send to the user", function() {
+                return im
+                    .outbound.send_to_user({
+                        endpoint: 'sms',
+                        content: 'hello!'
+                    })
+                    .then(function() {
+                        var sends = im.api.outbound.store;
+                        assert.equal(sends.length, 1);
+                        assert.deepEqual(sends[0], {
+                            to_addr: '+27987654321',
+                            content: 'hello!',
+                            endpoint: 'sms'
+                        });
+                    });
+            });
         });
     });
 });
