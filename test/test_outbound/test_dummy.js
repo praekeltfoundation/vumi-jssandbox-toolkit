7d326e64c6e07cd429807921bea8e223d252c092
justinvdm <justinvdm.0.1.0@gmail.com>
2014-03-03 10:49:08 +0200

    More test stubs for DummyOutboundResource

--- test/test_outbound/test_dummy.js
+++ test/test_outbound/test_dummy.js
@@ -18,18 +18,33 @@ describe("outbound.dummy", function() {
         describe(".handlers", function() {
             describe(".reply_to", function() {
                 it("should record the sent reply message");
+                it("should fail if 'content' isn't given");
+                it("should fail if 'content' isn't a string or null");
+                it("should fail if 'in_reply_to' isn't given");
+                it("should fail if 'continue_session' isn't boolean");
             });
 
             describe(".reply_to_group", function() {
                 it("should record the sent reply message");
+                it("should fail if 'content' isn't given");
+                it("should fail if 'content' isn't a string or null");
+                it("should fail if 'in_reply_to' isn't given");
+                it("should fail if 'continue_session' isn't boolean");
             });
 
             describe(".send_to_tag", function() {
                 it("should record the sent message");
+                it("should fail if 'to_addr' isn't a string");
+                it("should fail if 'content' isn't a string");
+                it("should fail if 'tag' isn't a string");
+                it("should fail if 'tagpool' isn't a string");
             });
 
             describe(".send_to_endpoint", function() {
                 it("should record the sent message");
+                it("should fail if 'to_addr' isn't a string");
+                it("should fail if 'content' isn't a string");
+                it("should fail if 'endpoint' isn't a string");
             });
         });
     });
