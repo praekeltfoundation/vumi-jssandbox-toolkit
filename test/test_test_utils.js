a221e1d4bafd1756405fcb415d74d06086aee854
justinvdm <justinvdm.0.1.0@gmail.com>
2014-03-04 11:07:24 +0200

    Add tests for test_utils.make_im()

--- test/test_test_utils.js
+++ test/test_test_utils.js
@@ -1,6 +1,9 @@
 var assert = require('assert');
 var vumigo = require("../lib");
+var fixtures = vumigo.fixtures;
 var test_utils = vumigo.test_utils;
+
+var App = vumigo.App;
 var DummyApi = vumigo.dummy.api.DummyApi;
 
 
@@ -18,6 +21,50 @@ describe("test_utils", function() {
                     assert(result.success);
                     assert.deepEqual(api.log.info, ['foo']);
                 });
+            });
+        });
+    });
+
+    describe(".make_im", function() {
+        it("should allow a DummyApi instance to be given", function() {
+            var api = new DummyApi({config: fixtures.config()});
+
+            return test_utils.make_im({
+                api: api
+            }).then(function(im) {
+                assert.strictEqual(im.api, api);
+            });
+        });
+
+        it("should allow options to be given for a new DummyApi", function() {
+            var config = fixtures.config();
+            config.name = 'test_foo';
+
+            return test_utils.make_im({
+                api: {config: config}
+            }).then(function(im) {
+                assert.deepEqual(im.api.config.store, config);
+            });
+        });
+
+        it("should allow the the im's message to be configured", function() {
+            var msg = fixtures.msg();
+            msg.content = 'foo';
+
+            return test_utils.make_im({
+                msg: msg
+            }).then(function(im) {
+                assert.strictEqual(im.msg, msg);
+            });
+        });
+
+        it("should allow the app to be given", function() {
+            var app = new App('foo');
+
+            return test_utils.make_im({
+                app: app
+            }).then(function(im) {
+                assert.strictEqual(im.app, app);
             });
         });
     });
