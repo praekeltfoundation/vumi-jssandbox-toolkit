3bf40f49b5b3b074a9155713219932aa1d50f72a
justinvdm <justinvdm.0.1.0@gmail.com>
2014-03-19 13:26:30 +0200

    Better testing for BookletState

--- test/test_states/test_booklet.js
+++ test/test_states/test_booklet.js
@@ -1,9 +1,11 @@
+var _ = require('lodash');
 var Q = require('q');
-var assert = require('assert');
 
 var vumigo = require('../../lib');
+var fixtures = vumigo.fixtures;
 var App = vumigo.App;
 var AppTester = vumigo.AppTester;
+var EndState = vumigo.states.EndState;
 var test_utils = vumigo.test_utils;
 var BookletState = vumigo.states.BookletState;
 
@@ -16,23 +18,106 @@ describe("states.booklet", function() {
             var app = new App('states:test');
 
             app.states.add('states:test', function(name) {
-                return new BookletState(name, {
-                    next: "next_state",
+                _.defaults(tester.data.opts, {
+                    next: "states:next",
                     pages: 3,
                     page_text: function(n) {
                         return Q("Page " + n + ".");
                     }
                 });
+
+                return new BookletState(name, tester.data.opts);
+            });
+
+            app.states.add('states:next', function(name) {
+                return new EndState(name, {
+                    text: 'You are on the next state.'
+                });
             });
 
             tester = new AppTester(app);
+            tester.data.opts = {};
         });
 
-        it("should display the first page when the user enters");
-        it("should not change state if bad input was given");
-        it("should go to the previous page if asked");
-        it("should go to the next page if asked");
-        it("should go to the next state if asked");
-        it("should translate the displayed content");
+        it("should display the first page when the user enters", function() {
+            return tester
+                .input()
+                .check.reply([
+                    "Page 0.",
+                    "1 for prev, 2 for next, 0 to end."
+                ].join('\n'))
+                .run();
+        });
+
+        it("should show the same page if bad input was given", function() {
+            return tester
+                .setup.user.state('states:test')
+                .input('3')
+                .check.reply([
+                    "Page 0.",
+                    "1 for prev, 2 for next, 0 to end."
+                ].join('\n'))
+                .run();
+        });
+
+        it("should go to the previous page if asked", function() {
+            return tester
+                .setup.user.state({
+                    name: 'states:test',
+                    metadata: {page: 1}
+                })
+                .input('1')
+                .check.reply([
+                    "Page 0.",
+                    "1 for prev, 2 for next, 0 to end."
+                ].join('\n'))
+                .run();
+        });
+
+        it("should go to the next page if asked", function() {
+            return tester
+                .setup.user.state({
+                    name: 'states:test',
+                    metadata: {page: 1}
+                })
+                .input('2')
+                .check.reply([
+                    "Page 2.",
+                    "1 for prev, 2 for next, 0 to end."
+                ].join('\n'))
+                .run();
+        });
+
+        it("should go to the next state if asked", function() {
+            return tester
+                .setup.user.state('states:test')
+                .input('0')
+                .check.reply("You are on the next state.")
+                .check.user.state('states:next')
+                .run();
+        });
+
+        it("should translate the displayed content", function() {
+            tester.data.opts = {
+                pages: 2,
+                page_text: function(i) {
+                    return [
+                        test_utils.$('hello'),
+                        test_utils.$('goodbye')
+                    ][i];
+                },
+                footer_text: test_utils.$('yes or no?')
+            };
+
+            return tester
+                .setup.config(fixtures.config())
+                .setup.user.lang('af')
+                .input()
+                .check.reply([
+                    "hallo",
+                    "ja of nee?"
+                ].join('\n'))
+                .run();
+        });
     });
 });
