2f3988e81299890f1fc4c7a3886aa90d3195211a
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-07 23:35:55 +0200

    Implement Opts

--- test/test_structs.js
+++ test/test_structs.js
@@ -4,9 +4,10 @@ var assert = require('assert');
 var vumigo = require('../lib');
 var structs = vumigo.structs;
 var Model = structs.Model;
+var Opts = structs.Opts;
 
 
-describe("models", function() {
+describe("structs", function() {
     describe("Model", function() {
         describe(".do.clear", function() {
             it("should clear all of the model's attributes", function() {
@@ -184,6 +185,131 @@ describe("models", function() {
                 var context = new Child();
                 assert.strictEqual(context, child_context);
                 assert.strictEqual(child_context, parent_context);
+            });
+        });
+    });
+
+    describe("Opts", function() {
+        it("should support option inheritance", function() {
+            var ThingOpts = Opts.extend().support('samuel');
+            var SubthingOpts = ThingOpts.extend().support('leroy');
+            var SubsubthingOpts = SubthingOpts.extend().support('jackson');
+
+            var opts = new SubsubthingOpts({
+                samuel: 'foo',
+                leroy: 'bar',
+                jackson: 'baz'
+            });
+
+            assert.equal(opts.samuel(), 'foo');
+            assert.equal(opts.leroy(), 'bar');
+            assert.equal(opts.jackson(), 'baz');
+        });
+
+        it("should support defaults", function() {
+            var ThingOpts = Opts.extend().support('samuel', {default: 'foo'});
+            var opts = new ThingOpts();
+            assert.equal(opts.samuel(), 'foo');
+        });
+
+        it("should support defaults inheritance", function() {
+            var ThingOpts = Opts
+                .extend()
+                .support('samuel', {default: 'foo'});
+
+            var SubthingOpts = ThingOpts
+                .extend()
+                .support('leroy', {default: 'bar'});
+
+            var SubsubthingOpts = SubthingOpts
+                .extend()
+                .support('leroy', {default: 'spam'})
+                .support('jackson', {default: 'baz'});
+
+            var opts = new SubthingOpts();
+            assert.equal(opts.samuel(), 'foo');
+            assert.equal(opts.leroy(), 'bar');
+
+            opts = new SubsubthingOpts();
+            assert.equal(opts.samuel(), 'foo');
+            assert.equal(opts.leroy(), 'spam');
+            assert.equal(opts.jackson(), 'baz');
+        });
+
+        it("should allow options to be given as values", function() {
+            var ThingOpts = Opts.extend().support('samuel');
+            var opts = new ThingOpts().samuel('foo');
+            assert.equal(opts.samuel(), 'foo');
+        });
+
+        it("should show allow options to be given as functions", function() {
+            var ThingOpts = Opts.extend().support('samuel');
+            var opts = new ThingOpts().samuel(function() {
+                return 'foo';
+            });
+
+            assert.equal(opts.samuel(), 'foo');
+        });
+
+        describe(".do.update", function() {
+            it("should set the given options", function() {
+                var ThingOpts = Opts
+                    .extend()
+                    .support('samuel')
+                    .support('leroy');
+
+                var opts = new ThingOpts();
+                opts.do.update({
+                    'samuel': 'foo',
+                    'leroy': 'bar'
+                });
+                assert.equal(opts.samuel(), 'foo');
+                assert.equal(opts.leroy(), 'bar');
+            });
+        });
+
+        describe(".do.set", function() {
+            it("should set the given option", function() {
+                var ThingOpts = Opts.extend().support('samuel');
+                var opts = new ThingOpts();
+                opts.do.set('samuel', 'foo');
+                assert.equal(opts.samuel(), 'foo');
+            });
+
+            it("should throw an error if an unsupported option is given",
+            function() {
+                assert.throws(function() {
+                    var opts = new Opts();
+                    opts.do.set('samuel', 'foo');
+                }, /Cannot set 'samuel', option not supported/);
+            });
+        });
+
+        describe(".do.get", function() {
+            it("should get the given option", function() {
+                var ThingOpts = Opts.extend().support('samuel');
+                var opts = new ThingOpts().samuel('foo');
+                assert.equal(opts.do.get('samuel'), 'foo');
+            });
+
+            it("should pass on the given arguments", function() {
+                var ThingOpts = Opts.extend().support('samuel');
+
+                var opts = new ThingOpts().samuel(function() {
+                    return Array.prototype.slice.call(arguments);
+                });
+
+                assert.deepEqual(
+                    opts.do.get('samuel', 'foo', 'bar'),
+                    ['foo', 'bar']);
+            });
+
+            it("should throw an error if an unsupported option is given",
+            function() {
+                assert.throws(function() {
+                    var opts = new Opts();
+                    opts.do.get('samuel');
+                }, /Cannot get 'samuel', option not supported/);
             });
         });
     });
