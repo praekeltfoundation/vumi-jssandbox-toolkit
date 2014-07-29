1d241b608d7adbf8434aea0e74102836b523bf92
justinvdm <justinvdm.0.1.0@gmail.com>
2014-07-29 23:29:38 +0200

    Implement basics for PaginatedState

--- test/test_states/test_paginated.js
+++ test/test_states/test_paginated.js
@@ -1,3 +1,7 @@
+var Q = require('q');
+var _ = require('lodash');
+var assert = require('assert');
+
 var vumigo = require('../../lib');
 var App = vumigo.App;
 var AppTester = vumigo.AppTester;
@@ -8,12 +12,22 @@ var PaginatedState = vumigo.states.PaginatedState;
 describe("states.paginated", function() {
     describe("PaginatedState", function() {
         var tester;
+        var opts;
+
+        function pager(i) {
+            return {
+                0: 'Page 0',
+                1: 'Page 1',
+                2: 'Page 2',
+            }[i];
+        }
 
         beforeEach(function () {
             var app = new App('states:test');
+            opts = function() { return {}; };
 
             app.states.add('states:test', function(name) {
-                return new PaginatedState(name, tester.data.opts);
+                return new PaginatedState(name, _.cloneDeep(opts));
             });
 
             app.states.add('states:next', function(name) {
@@ -21,7 +35,242 @@ describe("states.paginated", function() {
             });
 
             tester = new AppTester(app);
-            tester.data.opts = {};
+        });
+
+        it("should pass the correct args to the page function", function() {
+            opts.chars = 12;
+
+            opts.text = 'foo';
+
+            opts.page = function(i, text, n) {
+                assert([0, 1].indexOf(i) > -1);
+                assert.equal(text, 'foo');
+                assert.equal(n, 12);
+            };
+
+            return tester
+                .start()
+                .run();
+        });
+
+        describe("when on the first page", function() {
+            it("should show only the more and exit choices", function() {
+                opts.page = pager;
+
+                return tester
+                    .start()
+                    .check.reply([
+                        "Page 0",
+                        "1. More",
+                        "2. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should show more when asked", function() {
+                opts.page = pager;
+
+                return tester
+                    .inputs(null, '1')
+                    .check.reply([
+                        "Page 1",
+                        "1. Back",
+                        "2. More",
+                        "3. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should exit the state when asked", function() {
+                opts.page = pager;
+                opts.next = 'states:next';
+
+                return tester
+                    .inputs(null, '2')
+                    .check.user.state('states:next')
+                    .check.reply('You are on the next state.')
+                    .run();
+            });
+
+            it("should stay on the same page on invalid input", function() {
+                opts.page = pager;
+                opts.next = 'states:next';
+
+                return tester
+                    .inputs(null, '3', '4', 'a', 'b')
+                    .check.reply([
+                        "Page 0",
+                        "1. More",
+                        "2. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+        });
+
+        describe("when on the last page", function() {
+            it("should show only the back and exit choices", function() {
+                opts.page = pager;
+
+                return tester
+                    .inputs(null, '1', '2')
+                    .check.reply([
+                        "Page 2",
+                        "1. Back",
+                        "2. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should go back when asked", function() {
+                opts.page = pager;
+
+                return tester
+                    .inputs(null, '1', '2', '1')
+                    .check.reply([
+                        "Page 1",
+                        "1. Back",
+                        "2. More",
+                        "3. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should exit the state when asked", function() {
+                opts.page = pager;
+                opts.next = 'states:next';
+
+                return tester
+                    .inputs(null, '1', '2', '2')
+                    .check.user.state('states:next')
+                    .check.reply('You are on the next state.')
+                    .run();
+            });
+
+            it("should stay on the same page on invalid input", function() {
+                opts.page = pager;
+                opts.next = 'states:next';
+
+                return tester
+                    .inputs(null, '1', '2', '3', '4', 'a', 'b')
+                    .check.reply([
+                        "Page 2",
+                        "1. Back",
+                        "2. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+        });
+
+        describe("when not on the first or last page", function() {
+            it("should show the back, more and exit choices", function() {
+                opts.page = pager;
+
+                return tester
+                    .inputs(null, '1')
+                    .check.reply([
+                        "Page 1",
+                        "1. Back",
+                        "2. More",
+                        "3. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should go back when asked", function() {
+                opts.page = pager;
+
+                return tester
+                    .inputs(null, '1', '1')
+                    .check.reply([
+                        "Page 0",
+                        "1. More",
+                        "2. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should show more when asked", function() {
+                opts.page = pager;
+
+                return tester
+                    .inputs(null, '1', '2')
+                    .check.reply([
+                        "Page 2",
+                        "1. Back",
+                        "2. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+
+            it("should exit the state when asked", function() {
+                opts.page = pager;
+                opts.next = 'states:next';
+
+                return tester
+                    .inputs(null, '1', '3')
+                    .check.user.state('states:next')
+                    .check.reply('You are on the next state.')
+                    .run();
+            });
+
+            it("should stay on the same page on invalid input", function() {
+                opts.page = pager;
+                opts.next = 'states:next';
+
+                return tester
+                    .inputs(null, '1', '4', 'a', 'b')
+                    .check.reply([
+                        "Page 1",
+                        "1. Back",
+                        "2. More",
+                        "3. Exit",
+                    ].join('\n'))
+                    .run();
+            });
+        });
+
+        describe("default 'page' function", function() {
+            it("should display the words fitting on the page", function() {
+                opts.chars = 22;
+
+                opts.text = [
+                    "Lorem ipsum dolor sit amet,",
+                    "consectetur adipisicing elit."
+                ].join(' ');
+
+                return Q()
+                    .then(function() {
+                        return tester
+                            .start()
+                            .check.reply([
+                                "Lorem ipsum dolor sit",
+                                "1. More",
+                                "2. Exit",
+                            ].join('\n'))
+                            .run();
+                    })
+                    .then(function() {
+                        return tester
+                            .inputs(null, '1')
+                            .check.reply([
+                                "amet, consectetur",
+                                "1. Back",
+                                "2. More",
+                                "3. Exit",
+                            ].join('\n'))
+                            .run();
+                    })
+                    .then(function() {
+                        return tester
+                            .inputs(null, '1', '2')
+                            .check.reply([
+                                "adipisicing elit.",
+                                "1. Back",
+                                "2. Exit",
+                            ].join('\n'))
+                            .run();
+                    });
+            });
         });
     });
 });
